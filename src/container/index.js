a7acce63b8675da9e4253136e232a530bdc95267
Caleb Kyama Ndumbu <41087757+calebkyama@users.noreply.github.com>
2022-09-10 10:23:12 +0300

    import container components to app.js

---
+++ src/container/index.js
@@ -0,0 +1,6 @@
+export { defauut as About } from "./About/About";
+export { defauut as Footer } from "./Footer/Footer";
+export { defauut as Header } from "./Header/Header";
+export { defauut as Skills } from "./Skills/Skills";
+export { defauut as Testimonial } from "./Testimonial/Testimonial";
+export { defauut as Work } from "./Work/Work";
