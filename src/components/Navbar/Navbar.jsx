413d9f5a07212383f5996817246db2534b61c929
Caleb Kyama Ndumbu <41087757+calebkyama@users.noreply.github.com>
2022-09-13 11:40:37 +0300

    added header

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import { HiMenuAlt4, HiX } from "react-icons/hi";
 import { motion } from "framer-motion";
 
-import "./Navbar.scss";
 import { images } from "../../constants";
+import "./Navbar.scss";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
@@ -13,7 +13,6 @@ const Navbar = () => {
       <div className="app__navbar-logo">
         <img src={images.logo} alt="logo" />
       </div>
-
       <ul className="app__navbar-links">
         {["home", "about", "work", "skills", "contact"].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
@@ -22,8 +21,10 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
       <div className="app__navbar-menu">
         <HiMenuAlt4 onClick={() => setToggle(true)} />
+
         {toggle && (
           <motion.div
             whileInView={{ x: [300, 0] }}
@@ -45,4 +46,5 @@ const Navbar = () => {
     </nav>
   );
 };
+
 export default Navbar;
